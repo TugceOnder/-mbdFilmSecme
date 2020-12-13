535fb44ce8e0239076deb58fbae7261f658124d9
Tuğçe Önder <tugceonder775@gmail.com>
2020-12-13 15:55:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url="https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+
+html = requests.get(url).content
+soup= BeautifulSoup(html,"html.parser")
+
+list=soup.find("tbody",{"class":"lister-list"}).find_all("tr",limit=10)
+
+for tr in list:
+  title=tr.find("td",{"class":"titleColumn"}).find("a").text
+print(title)
